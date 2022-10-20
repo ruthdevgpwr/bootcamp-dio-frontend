5e666309ff8f6d731ec880946d87c86702f06e46
Ruth Dantas <ruthdevgp@gmail.com>
2022-10-20 17:13:06 -0300

    Criando arquivo do Terceiro Desafio do Curso para verificar com estruturas condicionais se um número é divisível por 5

---
+++ primeiras-paginas-interativas-js/primeiros-passos-js/estruturas-condicionais/desafios/terceiroDesafio.js
@@ -0,0 +1,10 @@
+// Verifique se um número é divisível por 5
+
+const numero = 20;
+const numeroDivisivelPor5 = numero % 5 === 0;
+
+if (numeroDivisivelPor5) {
+  console.log(`O número ${numero} é divisível por 5`);
+} else {
+  console.log(`O número ${numero} não é divisível por 5`);
+}
