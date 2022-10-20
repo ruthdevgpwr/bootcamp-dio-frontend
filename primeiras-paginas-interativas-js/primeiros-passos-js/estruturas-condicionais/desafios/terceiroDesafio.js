66a22558c1125b9848cfb698a40f9ec798976337
Ruth Dantas <ruthdevgp@gmail.com>
2022-10-20 17:15:59 -0300

    Acrescentando mais uma validacao na condicional if no terceiro desafio

--- primeiras-paginas-interativas-js/primeiros-passos-js/estruturas-condicionais/desafios/terceiroDesafio.js
+++ primeiras-paginas-interativas-js/primeiros-passos-js/estruturas-condicionais/desafios/terceiroDesafio.js
@@ -3,7 +3,9 @@
 const numero = 20;
 const numeroDivisivelPor5 = numero % 5 === 0;
 
-if (numeroDivisivelPor5) {
+if (numero === 0) {
+  console.log("O número é inválido");
+} else if (numeroDivisivelPor5) {
   console.log(`O número ${numero} é divisível por 5`);
 } else {
   console.log(`O número ${numero} não é divisível por 5`);
