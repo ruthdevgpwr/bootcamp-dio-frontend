3ea642488d119a3b15a5fb0fdb96330f138d987b
Ruth Dantas <ruthdevgp@gmail.com>
2022-10-20 17:57:52 -0300

    Adicionando primeiro desafio sobre funções no JS

---
+++ primeiras-paginas-interativas-js/funcoes-js/primeiroDesafio.js
@@ -0,0 +1,22 @@
+// Calcule o IMC de uma pessoa
+
+function calcularImc(peso, altura) {
+  return peso / Math.pow(altura, 2);
+}
+
+function classificarImc(imc) {
+  if (imc < 18.5) {
+    return "Abaixo do peso";
+  } else if (imc >= 18.5 && imc < 25) {
+    return "Peso normal";
+  } else if (imc >= 25 && imc < 30) {
+    return "Acima do peso";
+  } else if (imc >= 30 && imc < 40) {
+    return "Obeso";
+  } else {
+    return "Obesidade Grave";
+  }
+}
+
+const imc = calcularImc(150, 1.75);
+console.log(classificarImc(imc));
