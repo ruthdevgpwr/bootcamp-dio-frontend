40602dff10bafe602f78964792ed13e00f39ea33
Ruth Dantas <ruthdevgp@gmail.com>
2022-10-20 17:04:06 -0300

    Incluindo segundo desafio do curso - estruturas condicionais para verificar se um numero é par

---
+++ primeiras-paginas-interativas-js/primeiros-passos-js/estruturas-condicionais/desafios/segundoDesafio.js
@@ -0,0 +1,11 @@
+// Verifique se o número é par utilizando a estrutura condicional if
+
+const numero = 485;
+
+const isNumeroPar = numero % 2 === 0;
+
+if (isNumeroPar) {
+  console.log(`O número ${numero} é Par`);
+} else {
+  console.log(`O número ${numero} é Ímpar`);
+}
