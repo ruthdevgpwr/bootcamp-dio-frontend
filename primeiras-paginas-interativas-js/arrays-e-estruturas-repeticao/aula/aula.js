4037aefb6c199a9844646a12597b0be71a924d49
Ruth Dantas <ruthdevgp@gmail.com>
2022-10-20 23:01:45 -0300

    Adicionando codigos e exercicios resolucionados na aula de array e estruturas de repetição

---
+++ primeiras-paginas-interativas-js/arrays-e-estruturas-repeticao/aula/aula.js
@@ -0,0 +1,15 @@
+const alunos = ["João", "Vitor", "Marina"];
+
+//adiciona na ultima posicao do array
+alunos.push("Ruth");
+
+// remove elemento na última posição do Array
+alunos.pop();
+
+//remove o elemento na primeira posição do array - indice 0
+alunos.shift();
+
+console.log(alunos);
+
+//retorna o tamanho da lista/array
+alunos.length;
---
+++ primeiras-paginas-interativas-js/arrays-e-estruturas-repeticao/aula/exer01.js
@@ -0,0 +1,8 @@
+const nome = "Ruth Dantas";
+
+console.log(nome[0]);
+console.log(nome.length);
+
+for (i = 0; i < nome.length; i++) {
+  console.log(nome[i]);
+}
