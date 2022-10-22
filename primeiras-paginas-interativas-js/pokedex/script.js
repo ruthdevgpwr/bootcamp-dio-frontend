c18e8751c66d8d64cb9c2ca883cf6b75e50f27fa
Ruth Dantas <ruthdevgp@gmail.com>
2022-10-22 00:05:24 -0300

    reformando o código

--- primeiras-paginas-interativas-js/pokedex/script.js
+++ primeiras-paginas-interativas-js/pokedex/script.js
@@ -1,1 +1,12 @@
+const botaoTeste = document.getElementById("botao");
 
+function mudarBotao() {
+  botaoTeste.classList.toggle("botaoClicado");
+  if (botaoTeste.classList.contains("botaoClicado")) {
+    botaoTeste.innerHTML = "Clicou";
+  } else {
+    botaoTeste.innerHTML = "Clique";
+  }
+}
+
+botao.addEventListener("click", mudarBotao);
