0ed9b44520691cbccf4abb1ba437ee1a607ab39e
Ruth Dantas <ruthdevgp@gmail.com>
2022-10-23 00:53:34 -0300

    Realizando a primeira requisicao com fetch

--- primeiras-paginas-interativas-js/pokedex/assets/js/main.js
+++ primeiras-paginas-interativas-js/pokedex/assets/js/main.js
@@ -1,1 +1,18 @@
-console.log("JS");
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  })
+  .finally(() => {
+    console.log("Finished");
+  });
